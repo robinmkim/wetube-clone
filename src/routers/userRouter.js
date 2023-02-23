8097b175bbcb1567e6edfed4187b0c5be2b91f60
suppseob <59392588+suppseob@users.noreply.github.com>
2023-02-23 01:03:25 -0500

    until chapter 6

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userControllers";
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -13,11 +12,9 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use("/", globalRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => 
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
